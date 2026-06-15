22d7c71888dda1f69a3fec5d22a111eed11ef7db
unknown <chornogorskiyzhenya@gmail.com>
2026-06-15 17:33:04 +0300

    new

--- frontend/src/contexts/Login.tsx
+++ frontend/src/contexts/Login.tsx
@@ -27,7 +27,7 @@ export const LoginProvider = ({ children }:any) => {
   }, []);
   useEffect(() => {
     if (loaded && token ) { 
-      fetch('http://localhost:3000/api/verify', {
+      fetch('/api/verify', {
         headers: { Authorization: token }
       }) 
       .catch(function (error:any) {  
