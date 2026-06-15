9a0a35c28522c722863c58d17f3578eeee52efc5
unknown <chornogorskiyzhenya@gmail.com>
2026-06-15 16:28:04 +0300

    new

--- frontend/src/contexts/Login.tsx
+++ frontend/src/contexts/Login.tsx
@@ -27,7 +27,7 @@ export const LoginProvider = ({ children }:any) => {
   }, []);
   useEffect(() => {
     if (loaded && token ) { 
-      axios.get('https://localhost:3000/api/verify', {
+      fetch('https://localhost:3000/api/verify', {
         headers: { Authorization: token }
       }) 
       .catch(function (error:any) {  
